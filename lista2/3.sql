8fa6a61a25dbd9c8162ab0df03cf866e80dc5616
mateusz <mateusznasewicz@gmail.com>
2022-11-21 18:44:26 +0100

    zadanie 3 i 4

---
+++ lista2/3.sql
@@ -0,0 +1,65 @@
+INSERT INTO Producent (nazwa, kraj) VALUES ('Sanyo', 'China');
+INSERT INTO Producent (nazwa, kraj) VALUES ('Sony', 'China');
+INSERT INTO Producent (nazwa, kraj) VALUES ('Toshiba', 'China');
+INSERT INTO Producent (nazwa, kraj) VALUES ('Packard', 'China');
+INSERT INTO Producent (nazwa, kraj) VALUES ('Kodak', 'China');
+INSERT INTO Producent (nazwa, kraj) VALUES ('Leica', 'Japan');
+INSERT INTO Producent (nazwa, kraj) VALUES ('Nikon', 'Portugal');
+INSERT INTO Producent (nazwa, kraj) VALUES ('Olympus', 'Italy');
+INSERT INTO Producent (nazwa, kraj) VALUES ('Panasonic', 'Germany');
+INSERT INTO Producent (nazwa, kraj) VALUES ('Pentax', 'Argentina');
+INSERT INTO Producent (nazwa, kraj) VALUES ('Ricoh', 'Brazil');
+INSERT INTO Producent (nazwa, kraj) VALUES ('Samsung', 'England');
+INSERT INTO Producent (nazwa, kraj) VALUES ('Hewlet', 'Canada');
+INSERT INTO Producent (nazwa, kraj) VALUES ('Casio', 'USA');
+INSERT INTO Producent (nazwa, kraj) VALUES ('Canon', 'France');
+
+
+INSERT INTO Obiektyw (model, minPrzeslona, maxPrzeslona) VALUES ('AGH12', 1.1, 1.2);
+INSERT INTO Obiektyw (model, minPrzeslona, maxPrzeslona) VALUES ('ABC32', 2.21, 12.1);
+INSERT INTO Obiektyw (model, minPrzeslona, maxPrzeslona) VALUES ('HFH12', 123.123, 124.124);
+INSERT INTO Obiektyw (model, minPrzeslona, maxPrzeslona) VALUES ('OUI12', 832.12, 999.999);
+INSERT INTO Obiektyw (model, minPrzeslona, maxPrzeslona) VALUES ('AEFG', 123.123, 555.555);
+INSERT INTO Obiektyw (model, minPrzeslona, maxPrzeslona) VALUES ('AES', 78.3, 99.3);
+INSERT INTO Obiektyw (model, minPrzeslona, maxPrzeslona) VALUES ('UFJ', 234.123, 455.41);
+INSERT INTO Obiektyw (model, minPrzeslona, maxPrzeslona) VALUES ('ADF', 205.205, 305.305);
+INSERT INTO Obiektyw (model, minPrzeslona, maxPrzeslona) VALUES ('QWE', 99.4, 100.12);
+INSERT INTO Obiektyw (model, minPrzeslona, maxPrzeslona) VALUES ('IDOF', 45.5, 60.3);
+INSERT INTO Obiektyw (model, minPrzeslona, maxPrzeslona) VALUES ('OSKCU', 12.5, 20.5);
+INSERT INTO Obiektyw (model, minPrzeslona, maxPrzeslona) VALUES ('ASDDFG', 1.01, 99.12);
+INSERT INTO Obiektyw (model, minPrzeslona, maxPrzeslona) VALUES ('PQWOE', 787.1, 931.93);
+INSERT INTO Obiektyw (model, minPrzeslona, maxPrzeslona) VALUES ('ASJD', 12.12, 14.14);
+INSERT INTO Obiektyw (model, minPrzeslona, maxPrzeslona) VALUES ('ZZZ', 1299.12, 1999.14);
+
+
+INSERT INTO Matryca (przekatna, rozdzielczosc, typ) VALUES (21.23, 54.2, 'matryca1');
+INSERT INTO Matryca (przekatna, rozdzielczosc, typ) VALUES (21.22, 52.2, 'matryca2');
+INSERT INTO Matryca (przekatna, rozdzielczosc, typ) VALUES (21.27, 16.2, 'matryca3');
+INSERT INTO Matryca (przekatna, rozdzielczosc, typ) VALUES (21.24, 33.2, 'matryca4');
+INSERT INTO Matryca (przekatna, rozdzielczosc, typ) VALUES (21.23, 21.2, 'matryca5');
+INSERT INTO Matryca (przekatna, rozdzielczosc, typ) VALUES (25.21, 59.1, 'matryca6');
+INSERT INTO Matryca (przekatna, rozdzielczosc, typ) VALUES (51.23, 40.3, 'matryca7');
+INSERT INTO Matryca (przekatna, rozdzielczosc, typ) VALUES (61.29, 33.4, 'matryca8');
+INSERT INTO Matryca (przekatna, rozdzielczosc, typ) VALUES (81.21, 54.2, 'matryca9');
+INSERT INTO Matryca (przekatna, rozdzielczosc, typ) VALUES (71.28, 54.1, 'matryca10');
+INSERT INTO Matryca (przekatna, rozdzielczosc, typ) VALUES (12.27, 56.1, 'matryca11');
+INSERT INTO Matryca (przekatna, rozdzielczosc, typ) VALUES (21.25, 54.5, 'matryca12');
+INSERT INTO Matryca (przekatna, rozdzielczosc, typ) VALUES (25.24, 59.6, 'matryca13');
+INSERT INTO Matryca (przekatna, rozdzielczosc, typ) VALUES (26.23, 31.2, 'matryca14');
+INSERT INTO Matryca (przekatna, rozdzielczosc, typ) VALUES (27.21, 21.3, 'matryca15');
+
+INSERT INTO Aparat (model, producent, matryca, obiektyw, typ) VALUES ('Html', 1, 100, 1, 'kompaktowy');
+INSERT INTO Aparat (model, producent, matryca, obiektyw, typ) VALUES ('Python', 2, 101, 2, 'kompaktowy');
+INSERT INTO Aparat (model, producent, matryca, obiektyw, typ) VALUES ('Java', 3, 102, 3, 'kompaktowy');
+INSERT INTO Aparat (model, producent, matryca, obiektyw, typ) VALUES ('Css', 4, 103, 4, 'kompaktowy');
+INSERT INTO Aparat (model, producent, matryca, obiektyw, typ) VALUES ('Js', 5, 104, 5, 'kompaktowy');
+INSERT INTO Aparat (model, producent, matryca, obiektyw, typ) VALUES ('Django', 6, 105, 6, 'lustrzanka');
+INSERT INTO Aparat (model, producent, matryca, obiektyw, typ) VALUES ('Flask', 7, 106, 7, 'lustrzanka');
+INSERT INTO Aparat (model, producent, matryca, obiektyw, typ) VALUES ('Spring', 8, 107, 8, 'lustrzanka');
+INSERT INTO Aparat (model, producent, matryca, obiektyw, typ) VALUES ('React', 9, 108, 9, 'lustrzanka');
+INSERT INTO Aparat (model, producent, matryca, obiektyw, typ) VALUES ('Angular', 10, 109, 10, 'profesjonalny');
+INSERT INTO Aparat (model, producent, matryca, obiektyw, typ) VALUES ('Vue', 11, 110, 11, 'profesjonalny');
+INSERT INTO Aparat (model, producent, matryca, obiektyw, typ) VALUES ('Csharp', 12, 111, 12, 'profesjonalny');
+INSERT INTO Aparat (model, producent, matryca, obiektyw, typ) VALUES ('Cplusplus', 13, 112, 13, 'profesjonalny');
+INSERT INTO Aparat (model, producent, matryca, obiektyw, typ) VALUES ('Php', 14, 113, 14, 'inny');
+INSERT INTO Aparat (model, producent, matryca, obiektyw, typ) VALUES ('Kotlin', 15, 114, 15, 'inny');
