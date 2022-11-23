8ad1437c8f7d7446cd8ffe9cd7ed4e06df6adc86
mateusz <mateusznasewicz@gmail.com>
2022-11-23 13:32:02 +0100

    poprawa 4

--- lista2/4.sql
+++ lista2/4.sql
@@ -2,6 +2,7 @@ DELIMITER $$
 CREATE PROCEDURE IF NOT EXISTS GenerateAparat()
 BEGIN
     DECLARE i int;
+    DECLARE gen_model VARCHAR(30);
     DECLARE producent int;
     DECLARE matryca int;
     DECLARE obiektyw int;
@@ -10,12 +11,13 @@ BEGIN
     SET i = 1;
     
     gen: WHILE i <= 100 DO
+        SELECT model INTO gen_model FROM Aparat ORDER BY RAND() LIMIT 1;
         SELECT ID INTO producent FROM Producent ORDER BY RAND() LIMIT 1;
         SELECT ID INTO matryca FROM Matryca ORDER BY RAND() LIMIT 1;
         SELECT ID INTO obiektyw FROM Obiektyw ORDER BY RAND() LIMIT 1;
         SELECT typ INTO typx FROM Aparat ORDER BY RAND() LIMIT 1;
 
-        INSERT INTO Aparat (model, producent, matryca, obiektyw, typ) VALUES (CONCAT('generatedModel',i),producent,matryca,obiektyw,typx);
+        INSERT INTO Aparat (model, producent, matryca, obiektyw, typ) VALUES (CONCAT(gen_model,i),producent,matryca,obiektyw,typx);
 
         SET i = i + 1;
     END WHILE gen;
